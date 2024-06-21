9411c49728a8f937e12dea9587afb3b0bae62651
Muhammad Aliyan <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-06-21 12:24:35 +0500

    Complete Navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -26,7 +26,24 @@ const Navbar = () => {
             <span>E</span>-Store
           </h4>
         </div>
-        <div className="cart"></div>
+        <div className="cart">
+          <a href="">
+            <i className="fa-solid fa-shopping-cart"></i>
+            <span className="card__count">0</span>
+          </a>
+          <div className="card__details">
+            <p>You have 3 items in your cart</p>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+            </ul>
+            <h4>Total</h4>
+            <div className="additional__buttons">
+              <button className="primary__button">VIEW CART</button>
+              <button className="primary__button">CHECKOUT</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
